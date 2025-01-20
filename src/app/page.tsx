6ae0c37b9866aef1c91e0b7d46ded7fc7ea31f2a
bobmacneal <bob.macneal@workingcompany.com>
2025-01-20 11:12:15 -0600

    Make sure user signed in to use chat

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import Chat from "@/app/components/Chat"
-import { auth as getServerSession } from "@/auth";
+import {auth as getServerSession} from "@/auth";
 
 export default async function Home() {
   const session = await getServerSession()
   return (
     <div>
-      <h1 className="text-xl font-bold mb-3">Chatterbox</h1>
+      <h1 className="text-xl font-bold mb-3">Chat Box</h1>
       {
         (session?.user)
           ? <Chat />
